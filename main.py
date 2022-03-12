2a578b16750f75736a656931b2f6c6a2d2dfdbe8
duxevy <79513394+duxevy@users.noreply.github.com>
2022-03-12 16:38:44 +0300

    Lesson 2

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 import pygame
 
-pygame.init()
 
-screen_size = width, height = 800,600
-screen = pygame.display.set_mode(screen_size)
+class Window:
+    def __init__(self):
+        pygame.init()
+        self._running = True
+        self._size = self.width, self.height = 640, 480
+        self._display_surf_color = (0, 0, 0)
+        self._display_surf = pygame.display.set_mode(self._size)
+        self._display_surf.fill(self._display_surf_color)
+        self._clock = pygame.time.Clock()
 
-BLACK = (0, 0, 0)
-screen.fill(BLACK)
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
 
-size_of_block = height_block, width_block = 100, 100
-color = (152, 255, 152)
-body_block = pygame.Surface(size_of_block)
-body_block.fill(color)
+    def on_render(self):
+        self._display_surf.fill(self._display_surf_color)
+        pygame.display.flip()
 
-rect_of_block = body_block.get_rect()
+    def on_start(self):
+        while self._running:
+            self._clock.tick(60)
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_render()
+        Window.on_cleanup()
+
+    @staticmethod
+    def on_cleanup():
+        pygame.quit()
+
+
+class Block:
+    def __init__(self, size=(100, 100), color=(152, 255, 152)):
+        self.image = pygame.Surface(size)
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+
 
 rect_of_block.x = 350
 rect_of_block.y = 250
 
 screen.blit(body_block, rect_of_block)
-
-clock = pygame.time.Clock()
-
-run = True
-while run:
-    clock.tick(60)
-    pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-pygame.quit()
